6a4d571c5f726175f44d9308ce4fe11a7b4f9e5c
Dilukshan Bimsara <dilukshanbimsara@gmail.com>
2022-09-12 10:13:49 +0530

    get data to player details

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -6,6 +6,7 @@ const {
   select_contact,
   select_email,
   select_nic,
+  select_player,
 } = require("./user.service");
 const { compareSync } = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -178,20 +179,33 @@ module.exports = {
           });
         }
       }
+
+
     },
+
     
     );
 
-    
-    // const accessToken = jwt.sign({nic: body.email},process.env.TOKEN,{expiresIn: 60*60*24})
-    //              console.log("passwords are matched");
-    // return res.json({
-    //                     success: 1,
-    //                     data: "manager",
-    //                     message: body.password,
-    //                     token: accessToken
-    //                 });
   },
+
+  showPlayer:(req, res) => {
+    const data = req.body;
+    select_player(data, (err, results)=>{
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+              success: 0,
+              message: "Database connection error",
+              data: body,
+              err: err,
+            });
+          }
+
+          return res.status(200).json({
+            res_data:results,
+          });
+    });
+  }
 };
 
 
